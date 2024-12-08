534dedd1c509fce15b5d159db710dba4f1712dd5
Both Péter <93333993+bothpeter@users.noreply.github.com>
2024-12-08 14:29:26 +0100

    created major components
    created the Header Component and components for each page, also done the routing between them.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home.component').then((m) => m.HomeComponent);
+        },
+    },
+    {
+        path: 'login',
+        loadComponent: () => {
+            return import('./login/login.component').then((m) => m.LoginComponent);
+        },
+    },
+    {
+        path: 'settings',
+        loadComponent: () => {
+            return import('./settings/settings.component').then((m) => m.SettingsComponent);
+        },
+    },
+    {
+        path: 'contacts',
+        loadComponent: () => {
+            return import('./contacts/contacts.component').then((m) => m.ContactsComponent);
+        },
+    },
+    {
+        path: 'exercises',
+        loadComponent: () => {
+            return import('./exercises/exercises.component').then((m) => m.ExercisesComponent);
+        },
+    },
+    {
+        path: 'recipes',
+        loadComponent: () => {
+            return import('./recipes/recipes.component').then((m) => m.RecipesComponent);
+        },
+    },
+    {
+        path: 'calculator',
+        loadComponent: () => {
+            return import('./calculator/calculator.component').then((m) => m.CalculatorComponent);
+        },
+    },
+];
