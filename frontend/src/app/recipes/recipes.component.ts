5e32c9aafd9ddb4a9f9c4cadcbc93dbc5fc77594
Both Péter <93333993+bothpeter@users.noreply.github.com>
2025-01-27 17:44:54 +0100

    implemented popup window for foods
    also made api call for ingredients

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -11,6 +11,9 @@ import { CommonModule } from '@angular/common';
 
 export class RecipesComponent implements OnInit {
   food: any[] = [];
+  selectedFood: any = null;
+  ingredients: any[] = [];
+  showPopup: boolean = false;
 
   constructor() {}
 
@@ -35,4 +38,35 @@ export class RecipesComponent implements OnInit {
         console.error('Error fetching food:', error);
       });
   }
+
+  openPopup(food: any): void {
+    this.selectedFood = food;
+    this.showPopup = true;
+    this.fetchIngredients(food.food_id);
+  }
+
+  closePopup(): void {
+    this.showPopup = false;
+    this.selectedFood = null;
+    this.ingredients = [];
+  }
+
+  fetchIngredients(foodId: number): void {
+    const apiUrl = `http://127.0.0.1:8000/api/food_ingredients/${foodId}`;
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.ingredients = data.ingredients;
+        console.log(this.ingredients);
+      })
+      .catch((error) => {
+        console.error('Error fetching ingredients:', error);
+      });
+  }
+  
 }
