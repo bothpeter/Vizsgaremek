b51eaaa0ef1057267aadf9e323a22bf62926f5a6
Both Péter <93333993+bothpeter@users.noreply.github.com>
2025-02-11 19:00:50 +0100

    implemented footer component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { AuthService } from './services/auth.service';
 import { HeaderLoggedInComponent } from './components/header-logged-in/header-logged-in.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeaderLoggedInComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, HeaderLoggedInComponent, CommonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        if (!event.url.includes('#')) {
+          window.scrollTo(0, 0);
+        }
+      });
+  }
+}
