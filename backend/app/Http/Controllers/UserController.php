fc050280750c1dfc13fd50ad8761fedf7fe068e8
r-david8 <remesdavid8@gmail.com>
2025-02-19 21:33:54 +0100

    fixed other problems with user update

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -22,9 +22,9 @@ class UserController extends Controller implements HasMiddleware
 
     public function update_user(Request $request){
         $validator = Validator::make($request->all(), [
-            'name' => 'string|unique:users',
-            'email' => 'email|unique:users',
-            'password' => 'string',
+            'name' => 'string|unique:users|nullable',
+            'email' => 'email|unique:users|nullable',
+            'password' => 'string|nullable',
         ]);
 
         if ($validator->fails()) {
