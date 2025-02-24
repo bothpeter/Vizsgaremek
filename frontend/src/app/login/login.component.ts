d8db6d6649ee555a679d11281010df886d8be62d
Both Péter <93333993+bothpeter@users.noreply.github.com>
2025-02-24 18:15:09 +0100

    updated login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,58 +2,54 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink, HttpClientModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule, CommonModule, RouterLink],
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginError: string = '';
-  loginObj: Login;
+    loginError: string = '';
+    loginObj: Login = {
+        login: '',
+        password: ''
+    };
 
-  constructor(private http: HttpClient, private router: Router, private authService: AuthService) {
-    this.loginObj = new Login();
-  }
+    constructor(private apiService: ApiService, private router: Router, private authService: AuthService) { }
 
-  onSubmit() {
-    this.loginError = '';
+    onSubmit() {
+        this.loginError = '';
 
-    this.http.post('http://127.0.0.1:8000/api/login', this.loginObj).subscribe(
-      (res: any) => {
-        if (res.token != null) {
-          const userId = res.user.id;
-          const userName = res.user.name;
-          const userEmail = res.user.email;
-          const authToken = atob(res.token); // Decoded token from base64
-          this.authService.login(authToken, userId, userName, userEmail);
+        this.apiService.post('login', this.loginObj).subscribe({
+            next: (res: any) => {
+                if (res.token != null) {
+                    const userId = res.user.id;
+                    const userName = res.user.name;
+                    const userEmail = res.user.email;
+                    const authToken = atob(res.token); // Decoded token from base64
+                    this.authService.login(authToken, userId, userName, userEmail);
 
-          this.router.navigateByUrl('/');
-        } else {
-          console.log(res);
-        }
-      },
-      (error) => {
-        if (error.status === 401 && error.error.message === "Bad credentials") {
-          this.loginError = "Hibás email, név vagy jelszó.";
-        } else {
-          alert("Hiba történt a bejelentkezés során. Kérjük, próbáld újra később.");
-        }
-      }
-    );
-  }
+                    this.router.navigateByUrl('/');
+                } else {
+                    this.loginError = "Bejelentkezés sikertelen. Kérjük, próbálja újra.";
+                }
+            },
+            error: (error) => {
+                if (error.status === 401 && error.error.message === "Bad credentials") {
+                    this.loginError = "Hibás email, név vagy jelszó.";
+                } else {
+                    alert("Hiba történt a bejelentkezés során. Kérjük, próbáld újra később.");
+                }
+            }
+        });
+    }
 }
 
-export class Login {
-  login: string;
-  password: string;
-
-  constructor() {
-    this.login = '';
-    this.password = '';
-  }
+export interface Login {
+    login: string;
+    password: string;
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { ApiService } from './api.service';
 
