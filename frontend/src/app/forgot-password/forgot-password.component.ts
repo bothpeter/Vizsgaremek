bc759a202c221f41ea2868c69b5642bd88a38089
Both Péter <93333993+bothpeter@users.noreply.github.com>
2025-02-24 17:58:15 +0100

    updated forgot-password and reset-password

--- frontend/src/app/forgot-password/forgot-password.component.ts
+++ frontend/src/app/forgot-password/forgot-password.component.ts
@@ -2,37 +2,37 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from '../services/api.service';
 
 @Component({
-  selector: 'app-forgot-password',
-  standalone: true,
-  imports: [FormsModule, CommonModule, HttpClientModule, RouterLink],
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+    selector: 'app-forgot-password',
+    standalone: true,
+    imports: [FormsModule, CommonModule, RouterLink],
+    templateUrl: './forgot-password.component.html',
+    styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
-  email: string = '';
-  apiResponse: string = '';
+    resetEmail: string = '';
+    emailError: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+    constructor(private apiService: ApiService, private router: Router) { }
 
-  onSubmit() {
-    this.apiResponse = '';
-    const payload = { email: this.email };
+    onSubmit() {
+        this.emailError = '';
+        const payload = { email: this.resetEmail };
 
-    this.http.post('http://127.0.0.1:8000/api/forgot_password', payload).subscribe(
-      (res: any) => {
-        localStorage.setItem('resetEmail', this.email);
-        this.router.navigateByUrl('/reset-password');
-      },
-      (error) => {
-        if (error.status === 404 && error.error.message === "No record found") {
-          this.apiResponse = 'Hiba történt a kérés során. Kérjük, próbáld újra később.';
-        } else {
-          this.apiResponse = 'Az e-mail cím nem található';
-        }
-      }
-    );
-  }
+        this.apiService.post('forgot_password', payload).subscribe({
+            next: () => {
+                localStorage.setItem('resetEmail', this.resetEmail);
+                this.router.navigateByUrl('/reset-password');
+            },
+            error: (error) => {
+                if (error.status === 404 && error.error.message === "No record found") {
+                    this.emailError = 'Hiba történt a kérés során. Kérjük, próbáld újra később.';
+                } else {
+                    this.emailError = 'Az e-mail cím nem található';
+                }
+            }
+        });
+    }
 }
--- frontend/src/app/reset-password/reset-password.component.ts
+++ frontend/src/app/reset-password/reset-password.component.ts
@@ -1,91 +1,69 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { ValidationService } from '../services/validation.service';
 
 @Component({
-  selector: 'app-reset-password',
-  standalone: true,
-  imports: [FormsModule, CommonModule, HttpClientModule],
-  templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+    selector: 'app-reset-password',
+    standalone: true,
+    imports: [FormsModule, CommonModule],
+    templateUrl: './reset-password.component.html',
+    styleUrls: ['./reset-password.component.css']
 })
+
 export class ResetPasswordComponent {
-  token: string = '';
-  password: string = '';
-  password_confirmation: string = '';
-  apiResponse: string = '';
-  passwordError: string = '';
+    token: string = '';
+    password: string = '';
+    password_confirmation: string = '';
+    apiError: string = '';
+    passwordError: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+    constructor(private apiService: ApiService, private validationService: ValidationService, private router: Router) { }
 
-  onSubmit() {
-    this.apiResponse = '';
-    this.passwordError = '';
+    onSubmit() {
+        this.apiError = '';
+        this.passwordError = '';
 
-    if (this.password !== this.password_confirmation) {
-      this.passwordError = 'A jelszavak nem egyeznek.';
-      return;
-    }
-
-    const passwordValidation = this.validatePassword(this.password);
-    if (!passwordValidation.isValid) {
-      this.passwordError = passwordValidation.errorMessage;
-      return;
-    }
-
-    const email = localStorage.getItem('resetEmail');
-    const payload = {
-      email: email,
-      token: this.token,
-      password: this.password,
-      password_confirmation: this.password_confirmation
-    };
-
-    this.http.post('http://127.0.0.1:8000/api/reset_password', payload).subscribe(
-      (res: any) => {
-        this.apiResponse = res.message;
-        localStorage.removeItem('resetEmail');
-        this.router.navigateByUrl('/login');
-      },
-      (error) => {
-        if (error.status === 404 && error.error.message === "Token mismatch") {
-          this.apiResponse = 'Hiba történt a kérés során. Kérjük, próbáld újra később.';
-        } else {
-          this.apiResponse = 'A kód hibás vagy lejárt.';
+        const passwordValidation = this.validationService.validatePassword(this.password);
+        if (!passwordValidation.isValid) {
+            this.passwordError = passwordValidation.errorMessage;
+            return;
         }
-      }
-    );
-  }
 
-  validatePassword(password: string): { isValid: boolean, errorMessage: string } {
-    const minLength = 8;
-    const hasUpperCase = /[A-Z]/.test(password);
-    const hasNumber = /[0-9]/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+        const email = localStorage.getItem('resetEmail');
+        const payload = {
+            email: email,
+            token: this.token,
+            password: this.password,
+            password_confirmation: this.password_confirmation
+        };
 
-    if (password.length < minLength) {
-      return { isValid: false, errorMessage: 'A jelszónak legalább 8 karakter hosszúnak kell lennie.' };
-    }
-    if (!hasUpperCase) {
-      return { isValid: false, errorMessage: 'A jelszónak tartalmaznia kell legalább egy nagybetűt.' };
-    }
-    if (!hasNumber) {
-      return { isValid: false, errorMessage: 'A jelszónak tartalmaznia kell legalább egy számot.' };
-    }
-    if (!hasSpecialChar) {
-      return { isValid: false, errorMessage: 'A jelszónak tartalmaznia kell legalább egy speciális karaktert.' };
+        this.apiService.post('reset_password', payload).subscribe({
+            next: () => {
+                localStorage.removeItem('resetEmail');
+                this.router.navigateByUrl('/login');
+            },
+            error: (error) => {
+                if (error.status === 404 && error.error.message === "Token mismatch") {
+                    this.apiError = 'Hiba történt a kérés során. Kérjük, próbáld újra később.';
+                } else {
+                    this.apiError = 'A kód hibás vagy lejárt.';
+                }
+            }
+        });
     }
 
-    this.passwordError = '';
-    return { isValid: true, errorMessage: '' };
-  }
-
-  onPasswordChange() {
-    if (this.password === this.password_confirmation) {
-      this.passwordError = '';
+    onPasswordChange(): void {
+        if (this.password === this.password_confirmation) {
+            this.passwordError = '';
+        }
+        const passwordValidation = this.validationService.validatePassword(this.password);
+        if (!passwordValidation.isValid) {
+            this.passwordError = passwordValidation.errorMessage;
+        } else {
+            this.passwordError = '';
+        }
     }
-    this.validatePassword(this.password);
-  }
 }
