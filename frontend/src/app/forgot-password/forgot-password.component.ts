9d52c41127e0feca3ae4125eedb03a8f6dc9b57d
Both Péter <93333993+bothpeter@users.noreply.github.com>
2025-03-01 17:15:01 +0100

    added loading circle to loading password
    to prevent sending multiple emails

--- frontend/src/app/forgot-password/forgot-password.component.ts
+++ frontend/src/app/forgot-password/forgot-password.component.ts
@@ -1,32 +1,36 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-forgot-password',
     standalone: true,
-    imports: [FormsModule, CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, FormsModule],
     templateUrl: './forgot-password.component.html',
     styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
     resetEmail: string = '';
     emailError: string = '';
+    loading: boolean = false;
 
     constructor(private apiService: ApiService, private router: Router) { }
 
     onSubmit() {
+        this.loading = true;
         this.emailError = '';
         const payload = { email: this.resetEmail };
 
         this.apiService.post('forgot_password', payload).subscribe({
             next: () => {
+                this.loading = false;
                 localStorage.setItem('resetEmail', this.resetEmail);
                 this.router.navigateByUrl('/reset-password');
             },
             error: (error) => {
+                this.loading = false;
                 if (error.status === 404 && error.error.message === "No record found") {
                     this.emailError = 'Hiba történt a kérés során. Kérjük, próbáld újra később.';
                 } else {
