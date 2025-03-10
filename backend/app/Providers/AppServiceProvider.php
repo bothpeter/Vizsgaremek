9c46c4b82e3d9eb78f97e6b05071ee2f92c9facf
r-david8 <remesdavid8@gmail.com>
2025-03-10 16:03:00 +0100

    fixed sql fail

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         set_time_limit(3600);
+        DB::statement("SET GLOBAL max_allowed_packet = 100000000;");
     }
 }
